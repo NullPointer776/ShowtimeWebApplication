4081a9954403b97eb5f6d1cf99e637036f3a2bfe
he1322482639 <he1322482639@gmail.com>
2025-10-12 22:52:37 +1300

    Advance features Unit testing

--- ShowtimeUnitTesting/MovieListingUnitTest.cs
+++ ShowtimeUnitTesting/MovieListingUnitTest.cs
@@ -1,12 +1,59 @@
 using ShowtimeTestingProject.Controllers;
-
+using ShowtimeWebApplication.Models;
 namespace ShowtimeTestingProject;
-
+//Test class for MovieController Advance features (Bruce)
 [TestClass]
 public class MovieListingUnitTest
 {
     [TestMethod]
     public void TestSearchMethod()
+    {
+        MoviesUnitTestController controller = new MoviesUnitTestController();
+        var movies = controller.GetMovieList();
+        var searchString = "S";
+        var result = controller.Index(searchString, null, null);
+        var list=new List<Movie>
+        {
+            new Movie {Title = "SF1", Genre = Genre.ScienceFiction, Duration = 136  },
+            new Movie { Title = "SF2", Genre = Genre.ScienceFiction, Duration = 148 }
+        };
+        //Checking is the result contains the expected movies
+        CollectionAssert.Contains(list, result);
+    }
+    public void TestFilterMethod()
+    {
+        MoviesUnitTestController controller = new MoviesUnitTestController();
+        var movies = controller.GetMovieList();
+        var genreFilter = Genre.ScienceFiction;
+        var result = controller.Index(null, genreFilter.ToString(), null);
+
+        var list = new List<Movie>
+        {
+            new Movie {Title = "SF1", Genre = Genre.ScienceFiction, Duration = 136  },
+            new Movie { Title = "SF2", Genre = Genre.ScienceFiction, Duration = 148 }
+        };
+        Assert.IsTrue(result.Any(m => m.Title == "SF1"));
+        Assert.IsTrue(actualMovies.Any(m => m.Title == "SF2"));
+    }
+    public void TestSortAscendingMethod()
+    {
+        MoviesUnitTestController controller = new MoviesUnitTestController();
+        var movies = controller.GetMovieList();
+        var sortOrder = "";
+        var result = controller.Index(sortOrder,null,null);
+        var list = new List<Movie>
+        {
+            new Movie { Title = "A", Genre = Genre.Action, Duration = 152 },
+            new Movie {Title = "SF1", Genre = Genre.ScienceFiction, Duration = 136  },
+            new Movie { Title = "SF2", Genre = Genre.ScienceFiction, Duration = 148 }
+        };
+        for (int i=0;i<list.Count;i++)
+        {
+            Assert.IsTrue(list[i].Title, result[i].Title);
+        }
+        
+    }
+    public void TestSortDescendingMethod()
     {
         MoviesUnitTestController controller = new MoviesUnitTestController();
         var movies = controller.GetMovieList();
--- ShowtimeWebApplication/Controllers/MovieUnitTestController.cs
+++ ShowtimeWebApplication/Controllers/MovieUnitTestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ShowtimeWebApplication.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +12,37 @@ namespace ShowtimeTestingProject.Controllers
         {
             return new List<Movie>
             {
-                new Movie {Title = "The Matrix", Genre = Genre.ScienceFiction, Duration = 136  },
-                new Movie { Title = "Inception", Genre = Genre.ScienceFiction, Duration = 148 }
+                new Movie {Title = "SF1", Genre = Genre.ScienceFiction, Duration = 136  },
+                new Movie { Title = "SF2", Genre = Genre.ScienceFiction, Duration = 148 },
+                new Movie { Title = "A", Genre = Genre.Action, Duration = 152 }
             };
         }
-        public IActionResult Index()
+        public IActionResult Index(string sortOrder, string genreFilter, string searchString)
         {
             var movies = GetMovieList();
+            //Search
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = (List<Movie>)movies.Where(m => m.Title.Contains(searchString));
+            }
+            //Filter by genre
+            if (!string.IsNullOrEmpty(genreFilter))
+            {
+                var genre = Enum.Parse<Genre>(genreFilter);
+                movies = (List<Movie>)movies.Where(m => m.Genre == genre);
+            }
+            //Sort
+            movies = sortOrder == "title_desc" ? (List<Movie>)movies.OrderByDescending(m => m.Title) : (List<Movie>)movies.OrderBy(m => m.Title);
             return View(movies);
+        }
+        public IActionResult Details(int id)
+        {
+            var movie = GetMovieList().FirstOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
         }
     }
 }
